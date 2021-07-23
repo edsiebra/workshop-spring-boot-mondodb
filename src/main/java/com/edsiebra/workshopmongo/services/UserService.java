6aa10043c2e715c3c7c5992bfa554f52d3a7270e
Edson Siebra <edsiebra@gmail.com>
2021-07-23 08:31:13 -0300

    usando padrao dto para retornar usuarios

--- src/main/java/com/edsiebra/workshopmongo/services/UserService.java
+++ src/main/java/com/edsiebra/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.edsiebra.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.edsiebra.workshopmongo.domain.User;
 import com.edsiebra.workshopmongo.repository.UserRepository;
+import com.edsiebra.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,4 +19,11 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	
+	public User findByid(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+	}
+	
+	
 }
